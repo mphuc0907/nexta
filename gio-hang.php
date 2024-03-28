c185a6deb1c4e79acafe8af65342acd42abdbf79
Administrator <namnd@wecan-group.com>
2024-03-28 17:32:37 +0700

    two commit

---
+++ gio-hang.php
@@ -0,0 +1,68 @@
+<?php include "header.php" ?>
+    <main class="main">
+        <section class="section card-s1">
+            <div class="container">
+                <div class="title">
+                    <h2>Giỏ hàng</h2>
+                </div>
+                <div class="time-line">
+                    <div class="time-line-item">
+                        <div class="icon">
+                            <span>1</span>
+                        </div>
+                        <p>Giỏ hàng</p>
+                    </div>
+                    <div class="time-line-item">
+                        <div class="icon">
+                            <span>2</span>
+                        </div>
+                        <p>Chi tiết thanh toán</p>
+                    </div>
+                    <div class="time-line-item">
+                        <div class="icon">
+                            <span>3</span>
+                        </div>
+                        <p>Hoàn thành đơn hàng</p>
+                    </div>
+                </div>
+
+                <div class="total-gr">
+                    <div class="row">
+                        <div class="col-xl-7">
+                            <table class="table table-sm">
+                                <thead class="">
+                                <th scope="col">Sản phẩm</th>
+                                <th scope="col">Số lượng</th>
+                                <th scope="col">Giá tiền</th>
+                                <th scope="col">Tổng tiền</th>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col-xl-5">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+<?php include "footer.php" ?>
--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 	<link rel="stylesheet" type="text/css" href="dist/lib/slick/slick-theme.css">
 	<link rel="stylesheet" type="text/css" href="dist/lib/slick/slick.css">
 	<link rel="stylesheet" type="text/css" href="dist/assets/flickity.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="dist/assets/aos.css">
 	<link rel="stylesheet" type="text/css" href="dist/assets/custom.css">
 	<link rel="stylesheet" type="text/css" href="dist/assets/phuc.css">
